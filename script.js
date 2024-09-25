2d5f7442c6a60e219592f35d66c90446ad4ea5bc
Raj-Hawlader <rajhawlader385.rh@gmail.com>
2024-09-25 22:42:42 +0600

    functionality changed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector('#main'),
-//     smooth: true
-// })
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+})
 
 var fixed = document.querySelector("#fixed");
 var elemc = document.querySelector(".elems");
